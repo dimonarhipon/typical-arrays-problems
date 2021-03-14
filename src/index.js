5f3568a4958f66a0620b35b33140954b7cd0eff6
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2021-03-14 18:09:29 +0300

    init: start typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
+  if (array) {
+    if (array.length > 0) {
+      return Math.min(...array);    
+    }
+    return 0;  
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array) {
+    if (array.length > 0) {
+      return Math.max(...array);
+    }
+    return 0;
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array) {
+    if (array.length > 0) {          
+      const result = array.reduce((a, b) => a + b) / (array.length);
+      return result;
+    } 
+    return 0;
+  }
   return 0;
 }
